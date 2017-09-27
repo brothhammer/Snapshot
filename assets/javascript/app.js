6bda8f0b2b6c608b51e418688a4c55c5837f9ce6
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-26 19:35:54 -0500

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,15 @@
 $(document).ready(function()
 {
 
-
-      
-	//global variables
-	var cityPlaceId;
-
 var apiKeys =
 {
 	googleGeocode: "AIzaSyAtpm5B5wXU56SrAZ-Z9OBgfMoMaXovy3A",
 	googleStreetview: "AIzaSyDgH7bb2ybHKZ3K-45N_jNKOPAhB6p-5kc",
-	zillow: "X1-ZWz1fzqykaoah7_aui2x"
+	googlePlaces: "AIzaSyD5dj6sTAyGKxhzt9pd2gAo7Kp2JfOacLc",
+	zillow: "X1-ZWz1fzqykaoah7_aui2x",
+	onboard: "6f1057e839cf36ac5c35744c5461b18c",
+	foursquareClientID: "GTDR2JU024JPC111OI04WJT5NUZYF2FE5OI10LCSU2PRJEZ4",
+	foursquareClientSecret: "GTDR2JU024JPC111OI04WJT5NUZYF2FE5OI10LCSU2PRJEZ4"
 };
 
 var userAddress =
@@ -26,14 +25,14 @@ var userLocation =
 {
 	latitude: 0,
 	longitude: 0,
-	placeID: ""
+	placeID: "",
+	cityPlaceID: ""
 };
 
 
 $("#address_submit").click(function()
 {
 	event.preventDefault();
-
 
 	userAddress.street1 = $("#address_street1").val().trim().replace(/ /g, "+");
 	userAddress.street2 = $("#address_street2").val().trim();
@@ -44,39 +43,57 @@ $("#address_submit").click(function()
 	var geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userAddress.street1 + ",+" + 
 		userAddress.city + ",+" + userAddress.state + "&key=" + apiKeys.googleGeocode;
 
-	console.log(geocodeURL);
+	//console.log(geocodeURL);
 
 	$.ajax(
 	{
   		url: geocodeURL,
   		method: "GET",
-
 	})
 
 	.done(function(apireturn)
 	{
-		console.log(apireturn);
+		//console.log(apireturn);
 		userLocation.latitude = apireturn.results[0].geometry.location.lat;
 		userLocation.longitude = apireturn.results[0].geometry.location.lng;
 		userLocation.placeID = apireturn.results[0].place_id;
+		console.log(userLocation.placeID);
+		console.log(userLocation.latitude + "," + userLocation.longitude);
 
-		var steetviewURL = "https://maps.googleapis.com/maps/api/streetview?size=1000x300&location=" + 
+		var steetviewURL = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + 
 			userLocation.latitude + "," + userLocation.longitude + "&key=" +
 			apiKeys.googleStreetview;
 
-		console.log(steetviewURL);
+		//console.log(steetviewURL);
 
-		var streetViewImage = $("<img>").attr("src", steetviewURL).css("width", "600px").css("height", "400px")
-        streetViewImage.addClass("streetView");
+		var streetViewImage = $("<img>").attr("src", steetviewURL)
+		$("#streetview_results").prepend(streetViewImage);
+
+	})
+
+	var zillowURL = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=" + apiKeys.zillow + 
+		"&address=" + userAddress.street1 + "&citystatezip=" + userAddress.city +
+		"%2C+" + userAddress.state;
+	console.log("Zillow: " + zillowURL);
+
+	$.ajax(
+	{
+		url: zillowURL,
+		method: "GET",
+	}).done(function(zillowapireturn){
+		console.log(zillowapireturn);
+	})
 
 
 
-			
-		$("#streetview_results").append(streetViewImage);
-        $("#streetview_results").addClass("streetView");
 
 
-	})	
+
+
+
+
+
+
 
 	var geocodeCityURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
 		userAddress.city + ",+" + userAddress.state + "&key=" + apiKeys.googleGeocode;
@@ -86,66 +103,44 @@ $("#address_submit").click(function()
 		method: "GET",
 	})
 	.done(function(apireturn){
-		console.log(apireturn);
-		cityPlaceId = apireturn.results[0].place_id;
-		console.log(cityPlaceId);
+		//console.log(apireturn);
+		userLocation.cityPlaceID = apireturn.results[0].place_id;
+		//console.log(userLocation.cityPlaceID);
 	
 
 	logPlaceDetails();
 
-
-
 	function logPlaceDetails() {
-
-
-
           var service = new google.maps.places.PlacesService(document.getElementById('map'));
           service.getDetails({
-            placeId: cityPlaceId //'ChIJLwPMoJm1RIYRetVp1EtGm10'
+            placeId: userLocation.cityPlaceID //'ChIJLwPMoJm1RIYRetVp1EtGm10'
           }, function (place, status) {
-            console.log('Place details:', place);
+            //console.log('Place details:', place);
 
-            for(i=0;i<place.photos.length-1;i++){
-            	var photoURL = place.photos[i].getUrl({maxWidth: 300, maxHeight: 200})
+            for(i=0;i<place.photos.length;i++){
+            	var photoURL = place.photos[i].getUrl({maxWidth: 400, maxHeight: 400})
 
             	var addImg = $("<img>");
             	addImg.attr("src", photoURL);
 
-
-            	addImg.addClass("col s12 m4 l4");
-            	addImg.addClass("row");
-            	addImg.addClass("materialboxed")
-
             	$("#pictures").append(addImg);
-            	$("#pictures").addClass("row");
-            	$("#pictures").addClass("col s12 m4 l4");
-            	$("#pictures").addClass("materialboxed");
             }
           });
-}
+      }   //end of logPlaceDetails function
 
-
-initMap();
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('trafficMap'), {
-          zoom: 13,
-          center: {lat: userLocation.latitude, lng: userLocation.longitude}
-        });
-        var trafficLayer = new google.maps.TrafficLayer();
-        trafficLayer.setMap(map);
-
-        $("#trafficMap").css("height", "500px")
-
-
-      }
+});  //end of .done
 
 
 
-});
+})   //end of submit button click function
+
+
+$("#clear_button").click (function()
+{
+	window.location.reload(true);
 })
 
 
 
-})
 
-
+})   //end of ready
